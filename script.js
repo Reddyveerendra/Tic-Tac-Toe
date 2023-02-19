4a07d635490bb7aac78c3ed26bdb26d47d34377e
veerendra_cq_ms_10 <reddyveerendra535@gmail.com>
2023-02-19 19:33:33 +0530

    error correction

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ function createLayout() {
         cell.className = "cell";
         cell.id = `${i}`
         layout.appendChild(cell);
+        if (i % 2 != 0) {
+            cell.style.background = "white";
+            cell.style.color = "black";
+        }
+        else {
+            cell.style.background = "black";
+            cell.style.color = "white";
+        }
     }
     const cells = document.querySelectorAll(".cell");
     cells.forEach(cell => cell.addEventListener("click", handleClick));
@@ -24,10 +32,23 @@ function handleClick(e) {
         arr[parseInt(e.target.id)] = "O";
     }
     console.log(arr);
-    if (n == 8) {
+    n += 1;
+    if (((((arr[0] == "O" && (arr[3] == "O" && arr[6] == "O")) || (arr[1] == "O" && (arr[4] == "O" && arr[7] == "O"))) || (arr[2] == "O" && (arr[5] == "O" && arr[8] == "O"))) || (((arr[0] == "O" && (arr[1] == "O" && arr[2] == "O")) || (arr[3] == "O" && (arr[4] == "O" && arr[5] == "O"))) || (arr[8] == "O" && (arr[7] == "O" && arr[6] == "O")))) || ((arr[0] == "O" && (arr[4] == "O" && arr[8] == "O")) || (arr[2] == "O" && (arr[4] == "O" && arr[6] == "O")))) {
+        alert("Player 2 won the match")
         restart();
     }
-    n += 1;
+    else if ((((arr[0] == "X" && (arr[3] == "X" && arr[6] == "X")) || (arr[1] == "X" && (arr[4] == "X" && arr[7] == "X")) || (arr[2] == "X" && (arr[5] == "X" && arr[8] == "X"))) || (((arr[0] == "X" && (arr[1] == "X" && arr[2] == "X")) || (arr[3] == "X" && (arr[4] == "X" && arr[5] == "X"))) || (arr[8] == "X" && (arr[7] == "X" && arr[6] == "X")))) || ((arr[0] == "X" && (arr[4] == "X" && arr[8] == "X")) || (arr[2] == "X" && (arr[4] == "X" && arr[6] == "X")))) {
+        alert("Player 1 won the match");
+        restart();
+    }
+    else {
+        console.log("no");
+    }
+    if (n == 9) {
+        alert("Draw match")
+        restart();
+    }
+    console.log(n)
 }
 function restart() {
     while (layout.firstElementChild) {
